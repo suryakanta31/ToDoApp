1cda0545078d91c643aa9bcea2b52f041a79cc08
suryakanta31 <suryakantamuduli856@gmail.com>
2025-08-21 18:08:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import TodoApp from "./components/TodoApp.jsx";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Surya To‑Do </h1>
+        
+        <TodoApp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
