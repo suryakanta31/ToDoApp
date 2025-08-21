6c7164c4bb1e6ca4b32134720fd4385ef8646d2c
suryakanta31 <suryakantamuduli856@gmail.com>
2025-08-21 18:26:05 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
